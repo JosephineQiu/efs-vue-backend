2f064cec93575287dc4c670fb8bc1ededa263786
JosephineQiu <mqiu@unomaha.edu>
2019-11-14 22:35:29 -0600

    changed db backend

--- Django_Vue/settings.py
+++ Django_Vue/settings.py
@@ -87,8 +87,7 @@ WSGI_APPLICATION = 'Django_Vue.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
     }
 }
 
