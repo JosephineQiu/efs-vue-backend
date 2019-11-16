1745f8b2e4ba3e85a57ae8888ab42fb1ebde469b
JosephineQiu <mqiu@unomaha.edu>
2019-11-16 01:27:16 -0600

    modified staticfiles

--- Django_Vue/settings.py
+++ Django_Vue/settings.py
@@ -17,7 +17,7 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'http')
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -27,7 +27,7 @@ SECRET_KEY = 'fam4!#hnk8w_yoludqx+gyx2=cojz8l3_o8h&&_ct7hpw3q7fy'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://efs-django-vue.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -139,7 +139,7 @@ USE_TZ = True
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT,'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 
 
@@ -175,7 +175,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'dist/static'),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
 
 
